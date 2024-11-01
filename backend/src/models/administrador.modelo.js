2a242098329358c2b4c47339159287dcfa302a51
AlvaroGil93 <akolotronic93@hotmail.com>
2024-10-31 20:23:19 -0500

    adminstador modelo

---
+++ backend/src/models/administrador.modelo.js
@@ -0,0 +1,37 @@
+const adminSchema = new mongoose.Schema({
+    // Nombre de usuario único y requerido para identificar al administrador
+    username: { type: String, required: true, unique: true },
+  
+    // Correo electrónico  para el inicio de sesión y comunicación
+    email: { type: String, required: true, unique: true },
+  
+    // Contraseña del administrador
+    password: { type: String, required: true },
+  
+    // Rol del administrador que define el nivel de acceso('superadmin').
+    //  El administrador puede editar publicaciones de usuarios.
+    role: { type: String, enum: ['superadmin', 'editor', 'moderator'], default: 'editor' },
+  
+    // Aqui se define los permisos específicos del administrador
+    permissions: {
+      // Permiso para crear publicaciones
+      createPost: { type: Boolean, default: true },
+      // Permiso para editar publicaciones, activado por defecto
+      editPost: { type: Boolean, default: true },
+      // Permiso para eliminar publicaciones, desactivado por defecto
+      deletePost: { type: Boolean, default: false },
+      // Permiso para gestionar categorías en el blog, desactivado por defecto
+      manageCategories: { type: Boolean, default: false },
+      // Permiso para gestionar otros usuarios, desactivado por defecto esto ayuda a limitar el acceso es decir solo abra un administrador 
+      manageUsers: { type: Boolean, default: false },
+    },
+  
+    // Fecha de creación del administrador, se asigna automáticamente la fecha actual
+    createdAt: { type: Date, default: Date.now },
+  
+    // Fecha de última actualización del perfil del administrador, que puede actualizarse al realizar cambios
+    updatedAt: { type: Date }
+  });
+  
+
+export const AdminModel = mongoose.model("Admin", adminSchema);
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     userName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role:{type: String,default: 'administrador'}
+    role:{type: String,default: 'cliente'}
 
 });
 
