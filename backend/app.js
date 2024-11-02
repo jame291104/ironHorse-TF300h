85aef2e90be2f2b1477e3c90b8f6145c8e90c742
Jesús Morán Espinoza <jame291104@gmail.com>
2024-11-01 21:25:53 -0500

    error with connection

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+// Vamos a configurar nuestro servidor con Express y vamos a gestionar todo lo relacionado  con la logica de negocio -> conexion de base de datos, peticiones, respuestas
+// 1. Importamos las dependencias y modulos que necesitamos
+
+import express from "express";
+import dotenv from "dotenv"
+import { connectionMongo } from "./src/config/dataBase.js";
+
+// 2 configurar el uso de nuestro servidor
+const app = express()
+dotenv.config()
+connectionMongo()
+
+const port = process.env.PORT;
+
+app.use(express.json());
+
+// 3. Ejecutar el servidor en nuestro computador
+app.listen(port, () => {console.log("We are conected ironhorse", port)})
