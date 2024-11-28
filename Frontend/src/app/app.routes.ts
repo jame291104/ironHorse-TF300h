0a387a315c072268ad9542ebd6ad8893188d480d
juan <ospinajuanjesus3@gmail.com>
2024-11-27 21:59:17 -0500

    se crean paginas admin,inicio,login,productos,registro; se importan, y despues de esto se crean las rutas para cada una de ellas, estan funcionando con normalidad.

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { ProductosComponent } from './paginas/productos/productos.component';
+import { AdminComponent } from './paginas/admin/admin.component';
+import { RegistroComponent } from './paginas/registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:InicioComponent,title: 'inicio'},
+    {path: 'inicio sesion', component: LoginComponent, title: 'inicio sesion'},
+    {path: 'productos', component: ProductosComponent,title: 'productos'},
+    {path: 'panel admin', component: AdminComponent, title: ' panel admin'},
+    {path: 'registro', component: RegistroComponent, title: 'registro'}
+];
