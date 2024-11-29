7f4cc546580cc8ca1c4d6a3dfe321675de43fcb9
juan <ospinajuanjesus3@gmail.com>
2024-11-28 19:49:56 -0500

    hacemos usuarios y inventario dentro de admin

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -5,11 +5,20 @@ import { ProductosComponent } from './paginas/productos/productos.component';
 import { AdminComponent } from './paginas/admin/admin.component';
 import { RegistroComponent } from './paginas/registro/registro.component';
 import { NoFoundComponent } from './paginas/no-found/no-found.component';
+import { InventarioComponent } from './paginas/admin/inventario/inventario.component';
+import { UsuariosComponent } from './paginas/admin/usuarios/usuarios.component';
+
+
 export const routes: Routes = [
     {path: '', component:InicioComponent,title: 'inicio'},
     {path: 'inicio sesion', component: LoginComponent, title: 'inicio sesion'},
     {path: 'productos', component: ProductosComponent,title: 'productos'},
-    {path: 'panel admin', component: AdminComponent, title: ' panel admin'},
+    {path: 'admin', component: AdminComponent, title: ' panel admin', children:
+        [
+            {path:'inventario', component:InventarioComponent,title: 'inventario'},
+            {path:'usuarios',component:UsuariosComponent,title:'registro de usuarios'}
+        ]
+    },
     {path: 'registro', component: RegistroComponent, title: 'registro'},
     {path: '**', component: NoFoundComponent, title:'error 404'}
 ];
