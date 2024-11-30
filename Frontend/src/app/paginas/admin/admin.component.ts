8a72d800267f51c414a0333c98dbd469983d6294
juan jesus ospina piragua <ospinajuanjesus3@gmail.com>
2024-11-29 21:21:32 -0500

    se gregaron rutas

--- Frontend/src/app/paginas/admin/admin.component.ts
+++ Frontend/src/app/paginas/admin/admin.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink], //TENEMOS QUE PONER TOOD LO QUE IMPORTAMOS Y NECESITAMOS EN HTML
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
