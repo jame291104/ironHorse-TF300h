b0fd2f9b55b6a3915528a63c81f930e132984091
juan jesus ospina piragua <ospinajuanjesus3@gmail.com>
2024-12-06 18:23:35 -0500

    +juan

--- Frontend/src/app/paginas/no-found/no-found.component.ts
+++ Frontend/src/app/paginas/no-found/no-found.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NavBarComponent } from '../../componentes/nav-bar/nav-bar.component';
 @Component({
   selector: 'app-no-found',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './no-found.component.html',
   styleUrl: './no-found.component.css'
 })
