2030b14c3327ff2305ae8b6776e0b251f6380b9f
AlvaroGil93 <akolotronic93@hotmail.com>
2024-12-06 19:28:31 -0600

    about

--- Frontend/src/app/paginas/inicio/inicio.component.ts
+++ Frontend/src/app/paginas/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../componentes/nav-bar/nav-bar.component';
+import { ArticlesComponent } from "../admin/articles/articles.component";
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, ArticlesComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
