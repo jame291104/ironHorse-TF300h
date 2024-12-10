6f1c990eecbacc8721de295589dfb8cce84692ca
Jesús Morán Espinoza <jame291104@gmail.com>
2024-12-10 18:54:59 -0500

    cambios generales front y back

--- Frontend/src/app/services/publicacion.service.ts
+++ Frontend/src/app/services/publicacion.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from '../interfaces/publicacion';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublicacionService {
 
-  constructor() { }
+    //1. INYECCIÓN DE DEPENDENCIAS ---------------------------------------
+    private _httpClient = inject(HttpClient);
+
+    // 2. RUTA DE CONEXIÓN CON EL BACKEND ---------------------------------
+    private URL_PRODUCTOS = 'http://localhost:9000/publicaciones';
 }
