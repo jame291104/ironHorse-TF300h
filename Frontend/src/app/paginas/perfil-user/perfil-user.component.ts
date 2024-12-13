4d71056f5fb3b66c33866327ea63b3edd7fae011
AlvaroGil93 <akolotronic93@hotmail.com>
2024-12-13 16:10:15 -0600

    pagina para perfil de usuario

--- Frontend/src/app/paginas/perfil-user/perfil-user.component.ts
+++ Frontend/src/app/paginas/perfil-user/perfil-user.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../componentes/nav-bar/nav-bar.component';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-perfil-user',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [RouterLink],
   templateUrl: './perfil-user.component.html',
   styleUrl: './perfil-user.component.css'
 })
