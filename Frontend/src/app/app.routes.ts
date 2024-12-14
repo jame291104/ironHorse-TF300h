0e28753f087d946e87e990c87918001907a71226
Jesús Morán Espinoza <jame291104@gmail.com>
2024-12-13 21:19:05 -0500

    cambios guards e interceptors

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -14,27 +14,47 @@ import { ArticleComponent } from './paginas/blog/article/article.component';
 import { MeetingsComponent } from './paginas/blog/meetings/meetings.component';
 import { AboutUsComponent } from './paginas/about-us/about-us.component';
 import { PerfilUserComponent } from './paginas/perfil-user/perfil-user.component';
+import { authGuard } from './guards/auth.guard';
 //aca nos creamos las rutas para cada componente pagina
 export const routes: Routes = [
-    {path:'', component: InicioComponent, title: 'inicio'},
-    {path: 'perfil-user', component: PerfilUserComponent, title:'perfil-user'},
-    {path: 'registro', component: RegistroComponent, title: 'registro'},
-    {path: 'login', component: LoginComponent, title: 'inicio Sesion'},
-    {path: 'admin', component: AdminComponent, title: 'Panel admin', children:
-        [
-            {path:'articles', component:ArticlesComponent , title:'articles'},
-            {path:'usuarios',component:UsuariosComponent,title:'registro de usuarios'},
-            {path:'edit-articles', component:EditArticlesComponent, title:'edit-articles'}
-        ]},
-   
-    {path: 'blog-page', component: BlogComponent, title:'blog',children:
-        [
-            {path: 'article', component:ArticleComponent, title:'article'},
-            {path: 'meetings', component:MeetingsComponent, title:'meetings'}
-        ]
-    },
-    {path: 'sobre-nosotros', component:AboutUsComponent, title:'sobre nosotros'},
-    {path: '**', component: NoFoundComponent, title:'error 404'},
+  { path: '', component: InicioComponent, title: 'inicio' },
+  { path: 'perfil-user', component: PerfilUserComponent, title: 'perfil-user' },
+  { path: 'registro', component: RegistroComponent, title: 'registro' },
+  { path: 'login', component: LoginComponent, title: 'inicio Sesion' },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    title: 'Panel admin',
+    canActivate: [authGuard],
+    canActivateChild: [authGuard],
+    children: [
+      { path: 'articles', component: ArticlesComponent, title: 'articles' },
+      {
+        path: 'usuarios',
+        component: UsuariosComponent,
+        title: 'registro de usuarios',
+      },
+      {
+        path: 'edit-articles',
+        component: EditArticlesComponent,
+        title: 'edit-articles',
+      },
+    ],
+  },
 
+  {
+    path: 'blog-page',
+    component: BlogComponent,
+    title: 'blog',
+    children: [
+      { path: 'article', component: ArticleComponent, title: 'article' },
+      { path: 'meetings', component: MeetingsComponent, title: 'meetings' },
+    ],
+  },
+  {
+    path: 'sobre-nosotros',
+    component: AboutUsComponent,
+    title: 'sobre nosotros',
+  },
+  { path: '**', component: NoFoundComponent, title: 'error 404' },
 ];
-
