c9deff3906fdc364226c1e5231f1fd430d77f559
Jesús Morán Espinoza <jame291104@gmail.com>
2024-12-13 21:34:57 -0500

    guards is working

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,9 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideToastr } from "ngx-toastr"
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideToastr({
+      timeOut: 2000, //timepo de duración en pantalla
+      positionClass: "toast-bottom-right",
+      preventDuplicates: true,
+      easeTime: 0,
+      progressBar: true,
+    })
+  ]
 };
