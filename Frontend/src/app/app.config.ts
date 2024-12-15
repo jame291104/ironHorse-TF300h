44ae68860ecc421afc1f2a04e7ed4fe89b77bdaf
AlvaroGil93 <akolotronic93@hotmail.com>
2024-12-14 23:13:32 -0600

    animacion

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,9 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideToastr } from "ngx-toastr"
+import { provideToastr } from 'ngx-toastr';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
@@ -12,8 +11,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideToastr({
-      timeOut: 2000, //timepo de duración en pantalla
-      positionClass: "toast-bottom-right",
+      timeOut: 2000, // Tiempo de duración en pantalla
+      positionClass: 'toast-bottom-right',
       preventDuplicates: true,
       easeTime: 0,
       progressBar: true,
