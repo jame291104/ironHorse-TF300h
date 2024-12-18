05c8239214b7befee343d28f5d2f124a47f81660
Jesús Morán Espinoza <jame291104@gmail.com>
2024-12-18 18:18:29 -0500

    trying tiptap

---
+++ Frontend/src/app/componentes/tiptap/tiptap.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Editor } from '@tiptap/core';
+import StarterKit from '@tiptap/starter-kit';
+import Color from '@tiptap/extension-color';
+import ListItem from '@tiptap/extension-list-item';
+import TextStyle from '@tiptap/extension-text-style';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-tiptap',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './tiptap.component.html',
+  styleUrl: './tiptap.component.css'
+})
+
+
+export class TiptapComponent implements OnInit, OnDestroy {
+  editor: Editor | null = null;
+
+  ngOnInit() {
+    this.editor = new Editor({
+      extensions: [
+        Color.configure({ types: [TextStyle.name, ListItem.name] }),
+        // TextStyle.configure({ types: [ListItem.name]}),
+        StarterKit,
+      ],
+      content: `
+        <h2>Hi there,</h2>
+        <p>
+          this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+        </p>
+        <ul>
+          <li>
+            That’s a bullet list with one …
+          </li>
+          <li>
+            … or two list items.
+          </li>
+        </ul>
+        <p>
+          Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+        </p>
+        <pre><code class="language-css">body {  display: none;}</code></pre>
+        <p>
+          I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+        </p>
+        <blockquote>
+          Wow, that’s amazing. Good work, boy! 
+          <br />
+          — Mom
+        </blockquote>
+      `,
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.editor) {
+      this.editor.destroy();
+    }
+  }
+}
--- Frontend/src/app/paginas/admin/articles/articles.component.ts
+++ Frontend/src/app/paginas/admin/articles/articles.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TiptapComponent } from '../../../componentes/tiptap/tiptap.component';
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [],
+  imports: [TiptapComponent],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.css'
 })
