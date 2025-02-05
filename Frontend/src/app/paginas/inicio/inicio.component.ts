6a925ef4f0a1b079843a498bd42e0e5862252879
AlvaroGil93 <akolotronic93@hotmail.com>
2025-02-04 19:55:32 -0600

    modificacion banner

--- Frontend/src/app/paginas/inicio/inicio.component.ts
+++ Frontend/src/app/paginas/inicio/inicio.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../../componentes/nav-bar/nav-bar.component';
 import { ArticleCardComponent } from '../../componentes/single-components/article-card/article-card.component';
 import { FooterComponent } from '../../componentes/footer/footer.component';
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [NavBarComponent,FooterComponent, ArticleCardComponent],
+  imports: [NavBarComponent,FooterComponent, ArticleCardComponent,RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
